5e200816ad91736363d37c617f8a58f9e8aa77bc
Serverket <serverket@mailfence.com>
2023-11-12 02:31:00 -0400

    Persisting Dark Mode, moving gradient with lava lamp effect and Image Scheduling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,80 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/tailwind.css";
 import Church from "./assets/hermon-church.webp";
 import { Disclosure } from "@headlessui/react";
 
 function App() {
-  const [darkToggle, setDarkToggle] = React.useState(false);
+
+  /* Dark Mode */
+  const [darkToggle, setDarkToggle] = React.useState(
+    localStorage.getItem("darkMode") === "true" ? true : false
+  );
+
+  const toggleDarkMode = () => {
+    const newDarkMode = !darkToggle;
+    setDarkToggle(newDarkMode);
+    localStorage.setItem("darkMode", newDarkMode.toString());
+  };
+
+  /* Cover Schedule */
+  const image1Url = "/public/programas/jueves_8.jpg";
+  const image2Url = "/public/programas/domingos_1-250.jpg";
+  const [showImage, setShowImage] = useState(false);
+  const [currentImageUrl, setCurrentImageUrl] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+
+      // 
+      const displaySchedule1 = { day: 6, hours: 23, minutes: 54 };
+
+      // 
+      const displaySchedule2 = { day: 6, hours: 23, minutes: 55 };
+
+      const displaySchedules = [displaySchedule1, displaySchedule2];
+
+      for (const displaySchedule of displaySchedules) {
+        const displayTime = new Date(now);
+        displayTime.setHours(displaySchedule.hours);
+        displayTime.setMinutes(displaySchedule.minutes || 0);
+        displayTime.setSeconds(0);
+
+        // Check if the current time matches the specified display time
+        if (
+          now.getDay() === displaySchedule.day &&
+          now.getHours() === displaySchedule.hours &&
+          now.getMinutes() === displaySchedule.minutes
+        ) {
+          setCurrentImageUrl(
+            displaySchedule === displaySchedule1 ? image1Url : image2Url
+          );
+          setShowImage(true);
+
+          // Set a timeout to hide the image after 1 hour
+          setTimeout(() => {
+            setShowImage(false);
+            setCurrentImageUrl(null);
+          }, 60 * 60 * 1000); // 1 hour (60 minutes * 60 seconds * 1000 milliseconds)
+          return;
+        }
+      }
+
+      setShowImage(false);
+      setCurrentImageUrl(null);
+    }, 1000 * 60); // Check every minute
+
+    // Cleanup the interval when the component is unmounted
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <div className="flex items-center justify-center">
         <div className={`h-screen w-full ${darkToggle && "dark"}`}>
-          <div className="h-screen w-full flex items-center justify-center flex-col bg-gray-300 dark:bg-gray-800">
+          <div className="h-screen w-full flex items-center justify-center flex-col dark:bg-gray-800">
+            {/* original background in previous tag is: bg-gray-300 */}
             <img
               src="/charlie-brown.svg"
               className="absolute h-screen w-full bg-no-repeat bg-cover object-cover bg-center opacity-5 dark:opacity-10"
@@ -18,12 +82,14 @@ function App() {
             <label className="toggleDarkBtn">
               <input
                 type="checkbox"
-                onClick={() => setDarkToggle(!darkToggle)}
+                checked={darkToggle}
+                onChange={toggleDarkMode}
               />
               <span className="slideBtnTg bg-gray-400 round"></span>
             </label>
-            <div className="w-[370px] sm:w-96 overflow-hidden bg-gray-100 p-5 rounded-xl mt-4 text-white dark:bg-gray-900 transform filter backdrop-filter backdrop-blur-md bg-opacity-40 firefox:bg-opacity-50 opacity-80">
-              <div className="md:w-1/3 flex items-start justify-center">
+            {/* Previous opacity from next tag: bg-opacity-40 firefox:bg-opacity-50 opacity-80 */}
+            <div className="w-[370px] sm:w-96 overflow-hidden bg-gray-100 p-5 rounded-xl mt-4 text-white dark:bg-gray-900 transform filter backdrop-filter backdrop-blur-md bg-opacity-50">
+              <div className="w-full flex text-center items-center justify-center">
                 {/* <div id="fb-root"></div>
             <div className="fb-like-box mb-5 border-4 rounded-sm border-blue-500"
                data-href="https://m.facebook.com/people/Iglesia-Evangelica-Hermon/100064624004737"
@@ -33,11 +99,36 @@ function App() {
                data-stream="true" 
                data-show-border="true">
             </div> */}
+
+                {/*                 <div>
+                  {showImage ? (
+                    <div>
+                    <h1 className="text-slate-700 font-bold">Image Display at Specific Time</h1>
+                    <img className="rounded-lg h-52" src={currentImageUrl} alt="Displayed Image" />
+                    </div>
+                  ) : (
+                    <p className="text-slate-700 font-bold"></p>
+                  )}
+                </div> */}
               </div>
-              <div className="px-6 space-y-6 sm:space-y-8 text-center">
-                <div className="text-gray-800 dark:text-gray-200 font-bold text-3xl md:text-2xl">
+              <div className="px-6 space-y-4 sm:space-y-8 text-center">
+                <div className="text-gray-800 dark:text-gray-200 font-bold text-3xl md:text-2xl filter drop-shadow-md">
                   Radio Hermón
                 </div>
+                {/* <div className="flex flex-col items-center justify-center space-y-2">
+                  <span className="text-sm text-slate-700 dark:text-slate-600 font-bold">
+                    <span className="bg-green-300 dark:bg-slate-300 rounded-full mr-1">
+                      <i className="icon-podcast filter drop-shadow-md animate-pulse" />
+                    </span>
+                    Estás en sintonía de
+                  </span>
+                  <img
+                    className="rounded-lg h-52 md:h-40 lg:h-72 xl:h-80"
+                    src={image2Url}
+                    alt="Displayed Image"
+                  />
+                </div>
+                <p className="text-slate-700 font-bold"></p> */}
                 <div className="flex justify-center text-sm text-grey-darker">
                   <audio
                     className="w-64 sm:w-full rounded-full bg-gradient-to-t from-gray-200 to-gray-600 dark:bg-gradient-to-t dark:from-blue-500 dark:to-blue-100 dark:shadow-lg dark:shadow-blue-500/30 shadow-lg shadow-blue-500/50 border-[1px] border-blue-400 dark:border-none"
@@ -89,14 +180,14 @@ function App() {
                         src={Church}
                       />
                       <div className="ml-2 space-x-1">
-                      <i
-                        className={
-                          open
-                            ? "icon-angle-double-down animate-pulse"
-                            : "icon-home-circled"
-                        }
-                      />
-                      <span>La Iglesia</span>
+                        <i
+                          className={
+                            open
+                              ? "icon-angle-double-down animate-pulse"
+                              : "icon-home-circled"
+                          }
+                        />
+                        <span>La Iglesia</span>
                       </div>
                     </Disclosure.Button>
                     <Disclosure.Panel className="w-[370px] sm:w-96 rounded-l-xl rounded-r-xl rounded-bl-none rounded-br-none rounded-tl-none rounded-tr-none bg-gray-100 dark:bg-gray-900 transform filter backdrop-filter backdrop-blur-md bg-opacity-40 firefox:bg-opacity-50 opacity-80 px-4 pb-4">
@@ -142,7 +233,8 @@ function App() {
                         </a>
                       </div>
                       <div className="flex flex-row justify-center items-center mt-4 text-md text-gray-800 dark:text-gray-400 ">
-                        {`© ${new Date().getFullYear()}`} Fundación Cultural Hermón RTV
+                        {`© ${new Date().getFullYear()}`} Fundación Cultural
+                        Hermón RTV
                       </div>
                     </Disclosure.Panel>
                   </>
