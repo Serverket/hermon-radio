69319e9e7f3b37c990220e866555344147d19e26
Serverket <serverket@mailfence.com>
2023-11-19 01:03:27 -0400

    Bottom navbar improved.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import Church from "./assets/hermon-church.webp";
 import { Disclosure } from "@headlessui/react";
 
 function App() {
-
   /* Dark Mode */
   const [darkToggle, setDarkToggle] = React.useState(
     localStorage.getItem("darkMode") === "true" ? true : false
   );
-
   const toggleDarkMode = () => {
     const newDarkMode = !darkToggle;
     setDarkToggle(newDarkMode);
@@ -18,56 +16,8 @@ function App() {
   };
 
   /* Cover Schedule */
-  const image1Url = "/public/programas/jueves_8.jpg";
-  const image2Url = "/public/programas/domingos_1-250.jpg";
-  const [showImage, setShowImage] = useState(false);
-  const [currentImageUrl, setCurrentImageUrl] = useState(null);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const now = new Date();
-
-      // 
-      const displaySchedule1 = { day: 6, hours: 23, minutes: 54 };
-
-      // 
-      const displaySchedule2 = { day: 6, hours: 23, minutes: 55 };
-
-      const displaySchedules = [displaySchedule1, displaySchedule2];
-
-      for (const displaySchedule of displaySchedules) {
-        const displayTime = new Date(now);
-        displayTime.setHours(displaySchedule.hours);
-        displayTime.setMinutes(displaySchedule.minutes || 0);
-        displayTime.setSeconds(0);
-
-        // Check if the current time matches the specified display time
-        if (
-          now.getDay() === displaySchedule.day &&
-          now.getHours() === displaySchedule.hours &&
-          now.getMinutes() === displaySchedule.minutes
-        ) {
-          setCurrentImageUrl(
-            displaySchedule === displaySchedule1 ? image1Url : image2Url
-          );
-          setShowImage(true);
-
-          // Set a timeout to hide the image after 1 hour
-          setTimeout(() => {
-            setShowImage(false);
-            setCurrentImageUrl(null);
-          }, 60 * 60 * 1000); // 1 hour (60 minutes * 60 seconds * 1000 milliseconds)
-          return;
-        }
-      }
-
-      setShowImage(false);
-      setCurrentImageUrl(null);
-    }, 1000 * 60); // Check every minute
-
-    // Cleanup the interval when the component is unmounted
-    return () => clearInterval(interval);
-  }, []);
+  /*   const img1 = "/public/programas/jueves_8.jpg";
+       const img2 = "/public/programas/domingos_1-250.jpg"; */
 
   return (
     <>
@@ -99,34 +49,18 @@ function App() {
                data-stream="true" 
                data-show-border="true">
             </div> */}
-
-                {/*                 <div>
-                  {showImage ? (
-                    <div>
-                    <h1 className="text-slate-700 font-bold">Image Display at Specific Time</h1>
-                    <img className="rounded-lg h-52" src={currentImageUrl} alt="Displayed Image" />
-                    </div>
-                  ) : (
-                    <p className="text-slate-700 font-bold"></p>
-                  )}
-                </div> */}
               </div>
               <div className="px-6 space-y-4 sm:space-y-8 text-center">
                 <div className="text-gray-800 dark:text-gray-200 font-bold text-3xl md:text-2xl filter drop-shadow-md">
                   Radio Hermón
                 </div>
-                {/* <div className="flex flex-col items-center justify-center space-y-2">
+                {/*  <div className="flex flex-col items-center justify-center space-y-2">
                   <span className="text-sm text-slate-700 dark:text-slate-600 font-bold">
                     <span className="bg-green-300 dark:bg-slate-300 rounded-full mr-1">
                       <i className="icon-podcast filter drop-shadow-md animate-pulse" />
                     </span>
                     Estás en sintonía de
                   </span>
-                  <img
-                    className="rounded-lg h-52 md:h-40 lg:h-72 xl:h-80"
-                    src={image2Url}
-                    alt="Displayed Image"
-                  />
                 </div>
                 <p className="text-slate-700 font-bold"></p> */}
                 <div className="flex justify-center text-sm text-grey-darker">
@@ -174,11 +108,7 @@ function App() {
               <Disclosure>
                 {({ open }) => (
                   <>
-                    <Disclosure.Button className="py-4 px-4 w-[370px] sm:w-96 overflow-hidden rounded-l-full rounded-r-full rounded-bl-none rounded-br-none text-gray-800 dark:text-gray-200 font-bold text-lg md:text-xl bg-gray-100 dark:bg-gray-900 transform filter backdrop-filter backdrop-blur-md bg-opacity-40 firefox:bg-opacity-50 opacity-80">
-                      <img
-                        className="absolute inset-0 h-full w-1/3 object-cover"
-                        src={Church}
-                      />
+                    <Disclosure.Button className="py-2 px-4 w-[370px] sm:w-96 mb-2 overflow-hidden rounded-full text-gray-800 dark:text-gray-200 font-bold text-lg md:text-xl bg-gray-100 dark:bg-gray-900 transform filter backdrop-filter backdrop-blur-md bg-opacity-40 firefox:bg-opacity-50 opacity-80">
                       <div className="ml-2 space-x-1">
                         <i
                           className={
@@ -187,11 +117,17 @@ function App() {
                               : "icon-home-circled"
                           }
                         />
-                        <span>La Iglesia</span>
+                        <span className="filter drop-shadow-md">
+                          Iglesia Hermón
+                        </span>
                       </div>
+                      <img
+                        className="absolute opacity-10 transition-all ease-in-out duration-500 hover:opacity-100 blur-sm hover:blur-none inset-0 h-full w-full object-cover rounded-full"
+                        src={Church}
+                      />
                     </Disclosure.Button>
-                    <Disclosure.Panel className="w-[370px] sm:w-96 rounded-l-xl rounded-r-xl rounded-bl-none rounded-br-none rounded-tl-none rounded-tr-none bg-gray-100 dark:bg-gray-900 transform filter backdrop-filter backdrop-blur-md bg-opacity-40 firefox:bg-opacity-50 opacity-80 px-4 pb-4">
-                      <div className="flex flex-row items-center justify-center space-x-2 text-center pt-2">
+                    <Disclosure.Panel className="w-[370px] sm:w-96 rounded-bl-none rounded-br-none rounded-tl-3xl rounded-tr-3xl bg-gray-100 dark:bg-gray-900 transform filter backdrop-filter backdrop-blur-md bg-opacity-40 firefox:bg-opacity-50 opacity-80 px-4 pb-2">
+                      <div className="flex flex-row items-center justify-center space-x-2 text-center pt-4">
                         <a
                           href="https://www.facebook.com/people/Iglesia-Evangelica-Hermon/100064624004737/"
                           className="pointer-cursor"
@@ -232,7 +168,7 @@ function App() {
                           </button>
                         </a>
                       </div>
-                      <div className="flex flex-row justify-center items-center mt-4 text-md text-gray-800 dark:text-gray-400 ">
+                      <div className="flex flex-row justify-center items-center mt-2 text-md text-gray-800 dark:text-gray-400 ">
                         {`© ${new Date().getFullYear()}`} Fundación Cultural
                         Hermón RTV
                       </div>
